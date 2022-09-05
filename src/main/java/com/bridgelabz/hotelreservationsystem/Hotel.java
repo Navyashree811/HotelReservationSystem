aafc4dc1bf34214f20da2b7312062e18edde1dee
hp-pc <navyashree811@gmail.com>
2022-09-05 08:52:25 +0530

    Added : Add Hotel in Hotel Reservation System

---
+++ src/main/java/com/bridgelabz/hotelreservationsystem/Hotel.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.hotelreservationsystem;
+
+public class Hotel {
+	private String hotelName;
+	private int ratePerDay;
+
+	public String getHotelName() {
+		return hotelName;
+	}
+
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+
+	public int getRatePerDay() {
+		return ratePerDay;
+	}
+
+	public void setRatePerDay(int ratePerDay) {
+		this.ratePerDay = ratePerDay;
+	}
+
+	@Override
+	public String toString() {
+		return "Hotel [hotelName = " + hotelName + ", ratePerDay = " + ratePerDay + "]";
+	}
+
+}
--- src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationSystem.java
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservationSystem.java
@@ -1,8 +1,36 @@
 package com.bridgelabz.hotelreservationsystem;
 
-public class HotelReservationSystem {   
+import java.util.ArrayList;
+
+public class HotelReservationSystem {
+	static ArrayList<Hotel> HotelsList = new ArrayList<>();
+
+	// To view total HotelList
+	public static void view() {
+		for (Hotel i : HotelsList) {
+			System.out.println(i);
+		}
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Hotel Reservation System Problem");
+
+		Hotel hotelName1 = new Hotel();
+		hotelName1.setHotelName("Lakewood");
+		hotelName1.setRatePerDay(110);
+		HotelsList.add(hotelName1);
+
+		Hotel hotelName2 = new Hotel();
+		hotelName2.setHotelName("Bridgewood");
+		hotelName2.setRatePerDay(160);
+		HotelsList.add(hotelName2);
+
+		Hotel hotelName3 = new Hotel();
+		hotelName3.setHotelName("Ridgewood");
+		hotelName3.setRatePerDay(220);
+		HotelsList.add(hotelName3);
+
+		view();
+
 	}
 
 }
